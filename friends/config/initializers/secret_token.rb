f3216d440d9844ebf75d512a570fe24d61f39867
jacky <javasaloon@gmail.com>
2012-11-25 14:13:40 +0800

    add communityengine support

---
+++ friends/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Friends::Application.config.secret_token = '7120fc81e574b6e1650418efe24fd282e7031c2c91bc3c86d5f2468ccb5a0e62a88d7d754fc601b096440aaf977f83b22d2953d95f1b739dc81a48a1d6235850'
